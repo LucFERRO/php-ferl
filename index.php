afcac6f8e8cfe62cc281f89b2773cfe2d6130283
Luc FERRO <luchenri.ferro@gmail.com>
2021-07-15 10:07:23 +0200

    push n-1
    manque "responsive"

--- index.php
+++ index.php
@@ -11,13 +11,10 @@
                     if(!empty($_SESSION)) {                       //Si SESSION n'est pas vide, afficher la nav
                         include 'includes/ul.inc.html';
                         $table=$_SESSION['table'];
-                    } else {
-                        
-                    }
+                    } else {}
                 ?>
             </nav>
-            <section class="col-9 pr-5">
-                
+            <section class="col-9 pr-5">     
                     <?php 
                         if(isset($_POST['submit'])) {               
                             $_SESSION['table']=[                       
@@ -29,26 +26,18 @@
                             ];
                             echo '<h2>Données sauvegardées.</h2>';
                         //  print_r($_SESSION['table']);
+
                         } else if(isset($_GET['add'])) {
                             include 'includes/form.inc.html';
-                        // } else if((isset($_GET['debugging']) || (isset($_GET['concatenation']) || (isset($_GET['loop']) || (isset($_GET['function']) || (isset($_GET['delete'])) {
-                        //     $i=;         
-                        } else {?>
-                        <a href="index.php?add" type="button" class="btn btn-primary mb-2 gap-2">Ajouter des données</a>
-                        <?php
-                        }
+                        // } else if((isset($_GET['debugging']) || (isset($_GET['concatenation']) || (isset($_GET['loop']) || (isset($_GET['function']) || (isset($_GET['del'])) {       
 
-
-                        if(isset($_GET['debugging'])) {
+                        } else if(isset($_GET['debugging'])) {
                             echo "<h2>Débogage</h2><br>";
                             echo'<pre>';
-                            print_r($_SESSION['table']);              // ou       print nl2br(print_r($table, true));          SANS LES BALISES <pre>
+                            print_r($table);              // ou   ~~~~    print nl2br(print_r($table, true));          SANS LES BALISES <pre>
                             echo'</pre>';
-                          } else {
 
-                        }
-
-                        if(isset($_GET['concatenation'])) {
+                        } else if(isset($_GET['concatenation'])) {
                             echo "<h2>Concaténation</h2><br>";
                             //===> Construction d'une phrase avec le contenu du tableau
                             echo '<h3>',$table['first_name'],' ',$table['last_name'],'</h3>';
@@ -64,11 +53,8 @@
                             $table['size'] = number_format($table['size'], 2, ',', ' ');                //  number_format($number, decimals, decimals separator, thousands separator)
                             echo '<h3>',$table['first_name'],' ',$table['last_name'],'</h3>';
                             echo $table['age'],' ans, ','je mesure ',$table['size'], 'm et je fais partie des ', $table['situation'],'s de la promo Simplon.<br><br>';
-                        } else {
 
-                        }
-
-                        if(isset($_GET['loop'])) {
+                        } else if(isset($_GET['loop'])) {
                             echo "<h2>Boucle</h2><br>";
                             //===> Lecture du tableau à l'aide d'une boucle foreach
                             // echo "à la ligne n°0 correspond la clé first_name et contient ", $table['first_name'], "<br>";
@@ -81,11 +67,8 @@
                                 echo 'à la ligne n°',$i,' correspond la clé ', '"',$k,'"',' et contient ', '"',$v,'"', '<br>';
                                 ++$i;
                             }
-                        } else {
 
-                        }
-
-                        if(isset($_GET['function'])) {
+                        } else if(isset($_GET['function'])) {
                             echo "<h2>Fonction</h2><br>";
                             //===> Lecture du tableau à l'aide de la fonction readTable()
                             function readTable($t) {
@@ -96,24 +79,18 @@
                                 }
                             }
                             readTable($table);
-                            
-                            } else {
 
-                        }
-
-                        if(isset($_GET['delete'])) {                      // AJOUTER CONFIRMATION
+                        } else if(isset($_GET['del'])) {                      // AJOUTER CONFIRMATION
                         session_destroy();
                         echo '<h2>Les données ont bien été supprimées.</h2>';
-                        } else {
 
-                        }
-
+                        } else {?>
+                            <a href="index.php?add" type="button" class="btn btn-primary mb-2 gap-2">Ajouter des données</a>
+                            <?php
+                            }
                     ?>
-
             </section>
         </div>
-
-
     </section>
     <?php include 'includes/footer.inc.html' ?>
 </body>
